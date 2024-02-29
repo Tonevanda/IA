e94e67f9380ba223d7e1492a5d11fac39fc740ac
dratomitoma <tomasxavier03@gmail.com>
2024-02-29 21:15:18 +0000

    game no longer crashes on invalid moves

--- proj1/model/state.py
+++ proj1/model/state.py
@@ -11,12 +11,15 @@ class State:
     def make_move(self, pos, board):
         x, y = pos
         xs, ys = self.current_cell
-        board[y][x].extend(board[ys][xs])
-        board[ys][xs] = []
-        if self.current_player == self.player1:
-            self.current_player = self.player2
-        else:
-            self.current_player = self.player1
+        #if the move is valid, the pieces are moved and the turn changes
+        #the move is valid if the stack at the current position is not empty, the top piece is the current player's color, and the destination is in the list of possible moves
+        if(board[ys][xs]!= [] and board[ys][xs][-1]==self.current_player and pos in self.current_possible_moves):
+            board[y][x].extend(board[ys][xs])
+            board[ys][xs] = []
+            if self.current_player == self.player1:
+                self.current_player = self.player2
+            else:
+                self.current_player = self.player1
 
 
         
