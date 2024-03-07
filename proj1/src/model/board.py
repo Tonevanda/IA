9356f67689f9a6f23445d13db59cbc3e9cad8148
Tiago Cruz <tiagocruz057@gmail.com>
2024-03-07 17:49:22 +0000

    continuation

--- proj1/src/model/board.py
+++ proj1/src/model/board.py
@@ -12,6 +12,9 @@ class Board:
 
         self.current_possible_moves = None
     
+    def get_board(self):
+        return self.board
+
     def get_size(self):
         return self.size
     
@@ -69,31 +72,36 @@ class Board:
         self.board &= ~(STACK_MASK << (stack_position * 10))
         self.board |= (new_stack << (stack_position * 10))
 
+    def get_bitmap_position(self, row, col):
+        return row * self.size + col
 
     def make_hexagon(self):
         cut = int(self.size * 0.25)
-        total = self.get_total_cells()
-        
-        for i in range(total):
+        none_stack = STACK_MASK
 
         for i in range(self.size):
             for j in range(self.size):
+                bitmap_position = self.get_bitmap_position(i, j)
                 if i < cut and j < cut - i:
-                    self.board[i, j] = PIECE_NONE
+                    self.substitute_stack(bitmap_position, none_stack)
                 elif i < cut and j > self.size - (cut - i) - 1:
-                    self.board[i, j] = PIECE_NONE
+                    self.substitute_stack(bitmap_position, none_stack)
                 elif i >= self.size - cut and j < cut - (self.size - i - 1):
-                    self.board[i, j] = PIECE_NONE
+                    self.substitute_stack(bitmap_position, none_stack)
                 elif i >= self.size - cut and j > self.size - (cut - (self.size - i - 1)) - 1:
-                    self.board[i, j] = PIECE_NONE
-        print(self.board)
+                    self.substitute_stack(bitmap_position, none_stack)
 
     # Takes a position and returns the stack at that position
-    def get_stack(self, pos):
-        x, y = pos
-        if 0 <= x and x < self.size and 0 <= y and y < self.size:
-            return self.board[y][x]
-        return None
+    def get_stack(self, cell):
+        row, col = cell
+        bitmap_pos = self.get_bitmap_position(row, col)
+        return (self.board >> (bitmap_pos * 10)) & STACK_MASK
+
+    def is_none_stack(self, stack):
+        return stack == STACK_MASK
+    
+    def is_empty_stack(self, stack):
+        return (stack ^ STACK_MASK) == STACK_MASK
 
     def get_selected_cell(self):
         return self.selected_cell
@@ -102,24 +110,84 @@ class Board:
         if self.selected_cell is not None:
             return self.get_stack(self.selected_cell)
         return None
-
+    
+    def get_stack_size(self, stack):
+        return bin(stack).count("1")
+    
     # Takes a cell and returns a list of possible moves in the corresponding cell
+    # TODO: Better way using bit operations?
     def possible_moves(self, cell):
         x, y = cell
         #you can only move as many pieces as the height of the stack
         stack = self.get_stack(cell)
         if stack is not None:
-            max=len(stack)
+            max=self.get_stack_size(stack)
             # i and j are the offsets from the current position. They vary between -max and max+1
             # the condition 0<=x+i<self.size and 0<=y+j<self.size ensures that the move is within the board
             # the condition abs(j)<=max-abs(i) ensures that that diagonal moves aren't used (because they can't)
             # the condition self.board[y+j][x+i] != None ensures that the move is inside the hexagon
-            return [(x+i,y+j) for i in range(-max,max+1) for j in range(-max,max+1) if (i,j)!=(0,0) and 0<=x+i<self.size and 0<=y+j<self.size and abs(j)<=max-abs(i) and self.board[y+j][x+i] != None]
+            return [(x+i,y+j) for i in range(-max,max+1) for j in range(-max,max+1) if (i,j)!=(0,0) and 0<=x+i<self.size and 0<=y+j<self.size and abs(j)<=max-abs(i) and not self.is_none_stack(self.get_stack((y+j, x+i)))]
+
+    def remove_from_stack(self, stack, num_pieces):
+        stack_pieces = self.get_stack_size(stack)-1
+
+        for i in range(num_pieces):
+            stack &= ~(0b11 << ((stack_pieces-i)*2))
+
+        return stack & STACK_MASK
+
+    def get_removed_from_stack(self, stack, num_pieces):
+        removed = 0b0
+        stack_pieces = self.get_stack_size(stack)-1
+
+        for i in range(num_pieces):
+            piece_removed = (stack & (0b11 << ((stack_pieces-i)*2))) >> ((stack_pieces-i)*2)
+            removed <<= 2
+            removed |= piece_removed
+            stack &= ~(0b11 << ((stack_pieces-i)*2)) 
+        return removed
         
+    def add_pieces_to_stack(self, stack, new_stack):
+        shift = self.get_stack_size(stack) * 2
+        stack <<= shift
+        stack |= new_stack
+        return stack
+    
+    def get_distance_between_cells(self, source, destination):
+        return abs(destination[0] - source[0]) + abs(destination[1] - source[1])
+
+    def transfer_pieces(self, source, destination):
+        source_pos = self.get_bitmap_position(source[1], source[0])
+        destination_pos = self.get_bitmap_position(destination[1], destination[0])
+        print("Source: ", source_pos, "Destination: ", destination_pos)
+
+        distance = self.get_distance_between_cells(source, destination)
+        print("Distance: ", distance)
+
+        source_stack = self.get_stack(source)
+        destination_stack = self.get_stack(destination)
+
+        new_source_stack = self.remove_from_stack(source_stack, distance)
+        removed_stack = self.get_removed_from_stack(source_stack, distance)
+        new_destination_stack = self.add_pieces_to_stack(destination_stack, removed_stack)
+
+        print("New source stack: ", bin(new_source_stack))
+        print("Removed stack: ", bin(removed_stack))
+        print("New destination stack: ", bin(new_destination_stack))
+
+        self.substitute_stack(source_pos, new_source_stack)
+        self.substitute_stack(destination_pos, new_destination_stack)
+
+        self.stack_handling(self, destination[1], destination[0])
+
+
+
     def make_move(self, pos, board):
         x, y = pos
         xs, ys = self.selected_cell
-        current_player = self.game_state.get_current_player().get_color()
+        current_player = self.game_state.get_current_player()
+        selected_stack = self.get_stack(self.selected_cell)
+
         if (self.selected_cell == (0,0)): # Selected from own stack
             self.board[y][x].append(current_player)
             self.game_state.remove_from_player_stack()
@@ -127,30 +195,49 @@ class Board:
 
         #if the move is valid, the pieces are moved and the turn changes
         #the move is valid if the stack at the current position is not empty, the top piece is the current player's color, and the destination is in the list of possible moves
+        elif(not self.is_empty_stack(selected_stack), self.is_player_piece(self.selected_cell, current_player), pos in self.current_possible_moves):
+            self.transfer_pieces(self.selected_cell, pos)
+
         elif(board.board[ys][xs]!= [] and board.board[ys][xs][-1]==self.game_state.get_current_player().get_color() and pos in self.current_possible_moves):
             board.board[y][x].extend(board.board[ys][xs])
             board.board[ys][xs] = []
             self.stack_handling(board, x, y)
             
 
+    def handle_removed_pieces(self, removed_pieces):
+        for i in range(5):
+            piece = removed_pieces & (0b11 << (i*2))
+            if piece == self.game_state.get_current_player().get_color_bits():
+                self.game_state.add_stack_piece()
+
     #if the stack at the destination is higher than 5, the pieces are removed until it is 5 high
     #and if any of the pieces removed are the current player's color, they are added to the stack of the current player
     def stack_handling(self, board, x, y):
-        while len(board.board[y][x]) > 5:
-                if board.board[y][x].pop(0) == self.game_state.get_current_player().get_color():
-                    self.game_state.add_to_player_stack()
+        stack = self.get_stack((x, y))
+        full_stack = stack & STACK_MASK
+
+        if(self.get_stack_size(stack) > 5):
+            full_stack = stack & STACK_MASK
+            rest = stack & ~STACK_MASK
+            self.handle_removed_pieces(rest)
+            self.substitute_stack(self.get_bitmap_position(x, y), full_stack)
 
         if(not self.game_state.did_win()):
             self.game_state.next_turn()  
 
     # Given a cell, returns if it belongs to a given player
     def is_player_piece(self, cell, player):
-        (i, j) = cell
-        color = player.get_color()
-        return self.board[i][j] != None and self.board[i][j] != [] and self.board[i][j][-1] == color
+        
+        stack = self.get_stack(cell)
+        color = player.get_color_bits()
+        num_pieces = self.get_stack_size(stack)
+        if num_pieces == 0:
+            return False
+        return (stack&0b11) == color
 
     # Checks if the board has any pieces of a given player
     # TODO: before this, make "winnable move function" that verifies if the last move was a capture and didn't reveal a new opponent piece
+    # TODO: Do this also with bit operations
     def verify_lost(self, player):
         for i in range(self.size):
             for j in range(self.size):
--- proj1/src/view/boardView.py
+++ proj1/src/view/boardView.py
@@ -1,5 +1,5 @@
 import pygame
-from config import SCREEN_WIDTH, SCREEN_HEIGHT, CELL_SIZE
+from config import SCREEN_WIDTH, SCREEN_HEIGHT, CELL_SIZE, PIECE_ORANGE, PIECE_BLUE
 
 class BoardView:
     def __init__(self, board_model, starting_cell):
@@ -9,7 +9,29 @@ class BoardView:
         self.starting_cell_y = starting_cell_y
 
     def draw_board(self, window):
-        for i, row in enumerate(self.board.board):
+        for i in range(self.board.size):
+            for j in range(self.board.size):
+                stack = self.board.get_stack((i, j))
+                if(not self.board.is_none_stack(stack)):
+                    pygame.draw.rect(window, (0, 0, 0), (self.starting_cell_x + j * CELL_SIZE, self.starting_cell_y + i * CELL_SIZE, CELL_SIZE, CELL_SIZE), 2)
+                    for k in range(4,-1,-1):
+                        piece = stack & (0b11 << (k * 2))
+                        if piece == PIECE_ORANGE:
+                            color = (255, 100, 0)
+                            light_color = (255, 150, 50)
+                        elif piece == PIECE_BLUE:
+                            color = (50, 50, 255)
+                            light_color = (100, 100, 255)
+                        else:
+                            continue
+                        pygame.draw.circle(window, (0, 0, 0), (self.starting_cell_x + j * CELL_SIZE + CELL_SIZE // 2, self.starting_cell_y + i * CELL_SIZE + CELL_SIZE // 2 + k * -5), CELL_SIZE // 2 - 3)
+                        pygame.draw.circle(window, color, (self.starting_cell_x + j * CELL_SIZE + CELL_SIZE // 2, self.starting_cell_y + i * CELL_SIZE + CELL_SIZE // 2 + k * -5), CELL_SIZE // 2 - 5)
+                        pygame.draw.circle(window, light_color, (self.starting_cell_x + j * CELL_SIZE + CELL_SIZE // 2 - 5, self.starting_cell_y + i * CELL_SIZE + CELL_SIZE // 2 - 5 + k * -5), CELL_SIZE // 4)
+
+        """
+
+        for i, row in enumerate(self.board.size):
+            
             for j, cell in enumerate(row):
                 if cell is not None:
                     pygame.draw.rect(window, (0, 0, 0), (self.starting_cell_x + j * CELL_SIZE, self.starting_cell_y + i * CELL_SIZE, CELL_SIZE, CELL_SIZE), 2)
@@ -23,10 +45,9 @@ class BoardView:
                         pygame.draw.circle(window, (0, 0, 0), (self.starting_cell_x + j * CELL_SIZE + CELL_SIZE // 2, self.starting_cell_y + i * CELL_SIZE + CELL_SIZE // 2 + k * -5), CELL_SIZE // 2 - 3)
                         pygame.draw.circle(window, color, (self.starting_cell_x + j * CELL_SIZE + CELL_SIZE // 2, self.starting_cell_y + i * CELL_SIZE + CELL_SIZE // 2 + k * -5), CELL_SIZE // 2 - 5)
                         pygame.draw.circle(window, light_color, (self.starting_cell_x + j * CELL_SIZE + CELL_SIZE // 2 - 5, self.starting_cell_y + i * CELL_SIZE + CELL_SIZE // 2 - 5 + k * -5), CELL_SIZE // 4)
-
+        """
     def draw(self, window):
-        #self.draw_board(window)
-        pass
+        self.draw_board(window)
     
     """ Positional Drawing
     def generic_draw(self, stack, window, pos):
