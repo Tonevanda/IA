aedeeac222a70fc3817acea82c6331bbd6d28ee8
Tiago Cruz <tiagocruz057@gmail.com>
2024-03-25 23:07:54 +0000

    PLayer documentation

--- proj1/src/model/Player.py
+++ proj1/src/model/Player.py
@@ -9,59 +9,77 @@ class Player:
         self.stack_selected = False
         self.cells = np.empty((0, 2), int)
 
-    def get_cells(self):
+    # Get the cells that the player has pieces on
+    # TODO: Why when calling this, the cells don't work like they should
+    def get_cells(self) -> np.ndarray:
         return self.cells
     
-    def get_tuple_cells(self):
+    # Get the cells that the player has pieces on as a list of tuples
+    def get_tuple_cells(self) -> list[tuple[int, int]]:
         return [tuple(cell) for cell in self.cells]
     
-    def add_cell(self, cell):
-        cell = np.array(cell)  # Convert cell to a numpy array
-        if not any(np.array_equal(cell, existing_cell) for existing_cell in self.cells):
-            self.cells = np.append(self.cells, [cell], axis=0)
+    # Add a cell to the player's cells
+    def add_cell(self, cell: tuple) -> None:
+        cell = np.array(cell)  # Convert cell to a numpy array (i.e. (1,2) -> [1 2])
+        if not any(np.array_equal(cell, existing_cell) for existing_cell in self.cells): # If the cell is not already in the player's cells
+            self.cells = np.append(self.cells, [cell], axis=0) # Add the cell to the player's cells
 
-    def remove_cell(self, cell):
-        cell = np.array(cell)
-        self.cells = np.array([existing_cell for existing_cell in self.cells if not np.array_equal(cell, existing_cell)])
+    def remove_cell(self, cell: tuple) -> None:
+        cell = np.array(cell) # Convert cell to a numpy array (i.e. (1,2) -> [1 2])
+        self.cells = np.array([existing_cell for existing_cell in self.cells if not np.array_equal(cell, existing_cell)]) # Remove the cell from the player's cells
 
-    def get_player_type(self):
+    # Get the player type
+    def get_player_type(self) -> str:
         return self.player_type
     
-    def get_color(self):
+    # Get the player's color
+    def get_color(self) -> str:
         return self.color
     
-    def get_color_bits(self):
+    # Get the player's color as it's bit representation
+    def get_color_bits(self) -> int:
         return PIECE_ORANGE if self.color == 'Orange' else PIECE_BLUE
 
-    def get_stack_count(self):
+    # Get the amount of pieces the player has in their saved stack
+    def get_stack_count(self) -> int:
         return self.stack_pieces
     
-    def has_saved_pieces(self):
+    # Check if the player has any pieces in their saved stack
+    def has_saved_pieces(self) -> bool:
         return self.stack_pieces > 0
     
-    def add_stack_piece(self):
+    # Add a piece to the player's saved stack
+    def add_stack_piece(self) -> None:
         self.stack_pieces += 1
 
-    def remove_stack_piece(self):
+    # Remove a piece from the player's saved stack
+    def remove_stack_piece(self) -> None:
         self.stack_pieces -= 1
 
-    def select_stack(self):
+    # Selects the player's saved piece's stack
+    def select_stack(self) -> None:
         self.stack_selected = True
     
-    def unselect_stack(self):
+    # Unselects the player's saved piece's stack
+    def unselect_stack(self) -> None:
         self.stack_selected = False
 
-    def is_bot(self):
+    # Check if the player's a bot
+    def is_bot(self) -> bool:
         return self.player_type == 'Easy Bot' or self.player_type == 'Hard Bot' or self.player_type == 'Medium Bot'
 
-    def is_easy_bot(self):
+    # Check if the player's an easy bot
+    def is_easy_bot(self) -> bool:
         return self.player_type == 'Easy Bot'
 
-    def is_medium_bot(self):
+    # Check if the player's a medium bot
+    def is_medium_bot(self) -> bool:
         return self.player_type == 'Medium Bot'
     
-    def is_hard_bot(self):
+    # Check if the player's a hard bot
+    def is_hard_bot(self) -> bool:
         return self.player_type == 'Hard Bot'
 
-    def __str__(self):
+    # Defines the string representation of the player when printed
+    def __str__(self) -> str:
         return self.color + ' ' + self.player_type
