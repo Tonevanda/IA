e0c8a9a2b965ec6542207ee9460bfb96f1710117
Tiago Cruz <tiagocruz057@gmail.com>
2024-03-25 23:02:17 +0000

    State documentation

--- proj1/src/model/state.py
+++ proj1/src/model/state.py
@@ -2,33 +2,41 @@ from model.MainMenuState import MainMenuState
 from model.SelectMenuState import SelectMenuState
 from model.GameState import GameState
 from model.EndState import EndState
+from model.Player import Player
 
 class State:
     def __init__(self):
         self.current_state = MainMenuState(self)
 
-    def run(self, window):
+    # Run the current state
+    def run(self, window: object) -> None:
         self.current_state.run(window)
 
-    def to_select_menu(self):
-        if isinstance(self.current_state, MainMenuState):
-            self.current_state = SelectMenuState(self)
+    # Change the current state to the Select Menu state
+    def to_select_menu(self) -> None:
+        if isinstance(self.current_state, MainMenuState): # If the current state is the Main Menu state
+            self.current_state = SelectMenuState(self) # Change the current state to the Select Menu state
 
-    def start_game(self, size, Orange, Blue):
-        if isinstance(self.current_state, SelectMenuState):
-            self.current_state = GameState(self, size, Orange, Blue)
+    # Change the current state to the Game State
+    def start_game(self, size: int, Orange: 'Player', Blue: 'Player') -> None:
+        if isinstance(self.current_state, SelectMenuState): # If the current state is the Select Menu state
+            self.current_state = GameState(self, size, Orange, Blue) # Change the current state to the Game state
 
-    def to_end(self, winner):
-        if isinstance(self.current_state, GameState):
-            self.current_state = EndState(self, winner)
+    # Change the current state to the End state
+    def to_end(self, winner: 'Player') -> None: 
+        if isinstance(self.current_state, GameState): # If the current state is the Game state
+            self.current_state = EndState(self, winner) # Change the current state to the End state
 
-    def play_again(self):
-        if isinstance(self.current_state, EndState):
-            self.current_state = SelectMenuState(self)
+    # Change the current state to the Select Menu state after the game ends
+    def play_again(self) -> None:
+        if isinstance(self.current_state, EndState): # If the current state is the End state
+            self.current_state = SelectMenuState(self) # Change the current state to the Select Menu state
     
-    def to_quit(self):
+    # Quits the game by setting the current state as None that will be handled in main.py
+    def to_quit(self) -> None:
         self.current_state = None
 
-    def get_state(self):
+    # Get the current state
+    def get_state(self) -> 'State':
         return self.current_state
     
